9ee5eef3fe738180e31f3a046d46eceea7ea39a9
Traipoap <traipoap@hotmail.com>
2026-07-12 17:50:01 +0700

    feat: add allowedHosts for frontend.example.com domain

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,4 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({vite: {
+    server: {
+      allowedHosts: ['frontend.example.com', '.example.com'] // dev / server
+    },
+    preview: {
+      allowedHosts: ['frontend.example.com', '.example.com'] // preview
+    }
+  }
+});
