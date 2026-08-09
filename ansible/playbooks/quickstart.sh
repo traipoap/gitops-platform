895e163742fce4522c08ada2f15bbcc2bd1b9a72
Traipoap <traipoap@hotmail.com>
2026-08-09 22:30:40 +0700

    refacter: Secret hardcode but still keep exam secret

---
+++ ansible/playbooks/quickstart.sh
@@ -0,0 +1,6 @@
+ansible-playbook -i inventory/hosts playbooks/00-inventory.yml
+ansible-playbook -i inventory/hosts playbooks/01-cluster-setup.yml
+ansible-playbook -i inventory/hosts playbooks/02-servicemesh.yml
+ansible-playbook -i inventory/hosts playbooks/03-gitops-bootstrap.yml # Request user input for GitHub PAT
+ansible-playbook -i inventory/hosts playbooks/04-storage-networking.yml
+ansible-playbook -i inventory/hosts playbooks/05-garage-deploy.yml # Request user input for garage credentials (blank to generate)
--- ansible/playbooks/setup.sh
+++
@@ -1,5 +0,0 @@
-ansible-playbook -i inventory/hosts playbooks/01-cluster-setup.yml
-ansible-playbook -i inventory/hosts playbooks/02-servicemesh.yml
-ansible-playbook -i inventory/hosts playbooks/03-gitops-bootstrap.yml # Prompt for GitHub PAT
-ansible-playbook -i inventory/hosts playbooks/04-storage-networking.yml
-ansible-playbook -i inventory/hosts playbooks/05-garage-deploy.yml
