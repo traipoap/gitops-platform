d69f0f596ae6a8a15f676b648740f93bb811606c
Traipoap <traipoap@hotmail.com>
2026-08-13 00:01:26 +0700

    refactor: GitOps

--- ansible/playbooks/quickstart.sh
+++ ansible/playbooks/quickstart.sh
@@ -1,6 +1,6 @@
 ansible-playbook -i inventory/hosts playbooks/00-prerequisites.yml
 ansible-playbook -i inventory/hosts playbooks/01-cluster-setup.yml
 ansible-playbook -i inventory/hosts playbooks/02-servicemesh.yml
-ansible-playbook -i inventory/hosts playbooks/03-gitops-bootstrap.yml # Request user input for GitHub PAT
-ansible-playbook -i inventory/hosts playbooks/04-storage-networking.yml
-ansible-playbook -i inventory/hosts playbooks/05-garage-deploy.yml # Request user input for garage credentials (blank to generate)
+ansible-playbook -i inventory/hosts playbooks/03-storage-networking.yml
+ansible-playbook -i inventory/hosts playbooks/04-garage-deploy.yml # Request user input for garage credentials (blank to generate)
+ansible-playbook -i inventory/hosts playbooks/05-gitops-bootstrap.yml # Request user input for FluxCD and GitHub PAT
